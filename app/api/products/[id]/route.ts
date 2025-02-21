0cda940906c1bc221c804ca7671afcef49389b88
ervinpelonio8 <rufspelonio@gmail.com>
2025-02-21 17:13:30 +0800

    fix invalid GET params

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,34 +1,29 @@
-import { connectToDB } from "@/utils/database";
-import Product from "@/models/Product";
-import { NextResponse } from "next/server";
+import { connectToDB } from "@/utils/database"
+import Product from "@/models/Product"
+import { type NextRequest, NextResponse } from "next/server"
 
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const isValidObjectId = (id: string) => mongoose.Types.ObjectId.isValid(id);
+const isValidObjectId = (id: string) => mongoose.Types.ObjectId.isValid(id)
 
-export async function GET(
-  req: Request,
-  context: { params: { id: string } }
-) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   try {
-    await connectToDB();
+    await connectToDB()
 
-    if (!isValidObjectId(context.params.id)) {
-      return NextResponse.json({ error: "Invalid Product ID" }, { status: 400 });
+    if (!isValidObjectId(params.id)) {
+      return NextResponse.json({ error: "Invalid Product ID" }, { status: 400 })
     }
 
-    const product = await Product.findById(context.params.id);
+    const product = await Product.findById(params.id)
 
     if (!product) {
-      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+      return NextResponse.json({ error: "Product not found" }, { status: 404 })
     }
 
-    return NextResponse.json(product);
+    return NextResponse.json(product)
   } catch (error) {
-    console.error("Error fetching product:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch product" },
-      { status: 500 }
-    );
+    console.error("Error fetching product:", error)
+    return NextResponse.json({ error: "Failed to fetch product" }, { status: 500 })
   }
 }
+
